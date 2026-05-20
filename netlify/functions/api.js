0e051b041c2647815a3729fc4311e6389f0fdfc0
Babak <Babakdevgeek@gmail.com>
2026-05-20 14:37:51 +0330

    fix api missed returns and add async to handler

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,13 +1,25 @@
 import bot from "../../src/bot.js";
 
 /** @type {import("@netlify/functions").Handler} */
-export function handler(event) {
-    if (event.httpMethod === "POST") {
-        try {
-            const update = JSON.parse(event.body)
-            await bot.handleUpdate(update)
-        } catch (error) {
-            console.error("خطایی رخ داد", error)
+export async function handler(event) {
+    if (event.httpMethod !== "POST") {
+        return {
+            statusCode: 405,
+            body: "Method not allowed"
+        }
+    }
+    try {
+        const update = JSON.parse(event.body)
+        await bot.handleUpdate(update)
+        return {
+            statusCode: 200,
+            body: "OK"
+        }
+    } catch (error) {
+        console.error("خطایی رخ داد", error)
+        return {
+            statusCode: 500,
+            body: error
         }
     }
 }
