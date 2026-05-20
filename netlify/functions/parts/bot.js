0ba4eb87a22ee8a396a19eaffdc3337d5ac67b92
Babak <Babakdevgeek@gmail.com>
2026-05-20 15:25:42 +0330

    log 3

--- netlify/functions/parts/bot.js
+++ netlify/functions/parts/bot.js
@@ -3,7 +3,10 @@ import { setCommands } from "./commands.js";
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
+console.log("token is : ", process.env.BOT_TOKEN);
+
 setCommands(bot);
+console.log("code after set command run");
 
 
 export default bot;
